0d83d0cc01be0334fc277af5630f5025347fe80d
DavidM05 <78866055+DavidM05@users.noreply.github.com>
2021-03-12 08:31:12 -0500

    .

---
+++ servidor/src/controllers/home.js
@@ -0,0 +1,14 @@
+const ctrl = {};
+
+const { Image } = require('../model');
+
+
+ctrl.index = async (req, res) =>{
+    const images = await Image.find().sort({timestamp: -1});
+    res.render('index', {images});
+};
+
+
+
+
+module.exports = ctrl;
