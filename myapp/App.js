3a3968b95d51c9152693e323f5800cbf9159cf0f
DavidM05 <78866055+DavidM05@users.noreply.github.com>
2021-03-12 09:22:28 -0500

    aas

--- myapp/App.js
+++ myapp/App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class app extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      name: '',
+      edad: '',
+      user: '',
+      pass: ''
+    }
+  }
+  submit()
+  {
+    console.warn(this.state)
+  }
+  render() {
+    return (
+      <View style={{margin:20, marginTop:100}}>
+        <TextInput
+        placeholder="enter name"
+        onChangeText={(text) => this.setState({name: text})}
+        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
+        />
+        <TextInput
+        placeholder="edad"
+        onChangeText={(text) => this.setState({name: text})}
+        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
+        /><TextInput
+        placeholder="user"
+        onChangeText={(text) => this.setState({name: text})}
+        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
+        /><TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        onChangeText={(text) => this.setState({name: text})}
+        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
+        />
+        <Button title="Register" onpress={()=>{this.submit()}}/>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default app
