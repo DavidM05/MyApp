a279024a73bce48f01d24f5554d71fc7ae40a330
DavidM05 <78866055+DavidM05@users.noreply.github.com>
2021-04-22 14:26:39 -0500

    Update App.js

--- myapp/App.js
+++ myapp/App.js
@@ -1,47 +1,16 @@
 import React from 'react';
-import {View, Button, TextInput } from 'react-native';
+import Formu from './componetes/formu'
+import {View, Text, Button} from 'react-native'
+import HomeScreen from './componetes/Home/HomeScreen';
 
-class app extends React.Component {
-  constructor()
-  {
-    super();
-    this.state = {
-      name: '',
-      edad: '',
-      user: '',
-      pass: ''
-    }
-  }
-  submit()
-  {
-    console.warn(this.state)
-  }
-  render() {
-    return (
-      <View style={{margin:20, marginTop:100}}>
-        <TextInput
-        placeholder="enter name"
-        onChangeText={(text) => this.setState({name: text})}
-        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
-        />
-        <TextInput
-        placeholder="edad"
-        onChangeText={(text) => this.setState({name: text})}
-        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
-        /><TextInput
-        placeholder="user"
-        onChangeText={(text) => this.setState({name: text})}
-        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
-        /><TextInput
-        placeholder="password"
-        secureTextEntry={true}
-        onChangeText={(text) => this.setState({name: text})}
-        style={{borderwidth: 2, bordercolor: 'skyblue', margin: 20}}
-        />
-        <Button title="Register" onpress={()=>{this.submit()}}/>
-      </View>
-    )
-  }
+funtion HomeScreen= ({ navigation }){
+  return (
+  <View>
+    <Text>Home</Text>
+    <Button
+    title="Home"onPress= {() => navigation.navigate('./componetes/Home/HomeScreen')}
+    />
+  </View>
+  )
 }
-
 export default app
